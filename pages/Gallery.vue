92b560f10934cba679ad7980dabbfb6419cec5ee
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-07-14 18:15:45 +0200

    Showing gallery with useAsyncData

---
+++ pages/Gallery.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const { data: gallery, refresh } = await useAsyncData('get', () => 
+  $fetch(`https://picsum.photos/v2/list`) );
+console.log(gallery)
+
+</script>
+
+<template>
+  <h2>Gallery</h2>
+  <ul>
+    <li v-for="(image, index) in gallery" :key="`image-${index}`"> 
+    {{ image.id }}
+    {{ image.author }}
+    <img :src="image.download_url" width="100"/>
+    </li>
+  </ul>
+</template>
