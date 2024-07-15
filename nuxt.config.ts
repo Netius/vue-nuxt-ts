14c3fd50744ff47b82ad4c35eed640c1c4926f75
Fabio <fabio@axicon.no>
2024-07-15 13:30:05 +0200

    Added some link about bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// https://www.docs4.dev/posts/how-to-add-bootstrap-5-to-nuxt-3-a-beginner-s-guide
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
