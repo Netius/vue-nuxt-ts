bdf54b3a6c5fd64c102c788f4449432f93e0f355
Fabio <fabio@axicon.no>
2024-07-15 13:28:56 +0200

    Add some description

--- pages/user/post.vue
+++ pages/user/post.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Post children</h1>
+  <p>Showing a nested link element</p>
 </template>
--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Profile children</h1>
+  <p>Showing a nested link element</p>
 </template>
