061db016a9754e17858e01dbb213c4acc50fe2bc
Fabio <fabio@axicon.no>
2024-07-15 12:37:14 +0200

    Deleted unused pages

--- components/AppNavigation.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>Navigations</h2>
-</template>
--- layouts/BlogLayout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h2>This is my blog layout</h2>
-    <!-- <AppNavigation />
-      <NuxtPage /> 
-    <AppFooter/> -->
-  </div>
-</template>
