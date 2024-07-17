2d8c22daafcd6bb6bd2f0b52e4b9c8379f32c616
Fabio <fabio@axicon.no>
2024-07-17 15:42:12 +0200

    Added index as homepage

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Learning project</h1>
+  <p class="h5">Build with Vue3 , Nuxt3, Typescript, Bootstrap 5 og deployed i Google Cloud - App Engine</p>
+  <p>Testing differents modules and plugins</p>
+
+
+  <p class="mb-0 pt-3 h6" >Github</p>
+  <a href="https://github.com/Netius/vue-nuxt-ts" target="_blank"class="link-primary">https://github.com/Netius/vue-nuxt-ts</a>
+
+</template>
