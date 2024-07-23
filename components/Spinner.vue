56058d46fce8bb4abbe9f6ad273721f4d72c9aca
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-07-23 23:51:11 +0300

    Add Spinner to prj

---
+++ components/Spinner.vue
@@ -0,0 +1,12 @@
+
+<script setup lang="ts">
+  defineProps({
+      isLoading: Boolean,
+    })
+</script>
+
+<template>
+  <div v-if="isLoading" class="spinner-border" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
